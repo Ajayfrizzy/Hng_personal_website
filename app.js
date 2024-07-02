4a40a22e16838bfc348850c0c5b958b5f4bb53ba
ajayfrizzy <ajayholuwaseun@gmail.com>
2024-07-02 14:35:33 +0100

    My first HNG project

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let dates = new Date();
-let weekly = {weekday: "long"};
-let day = dates.toLocaleDateString('en-Ng', weekly)
-let date = document.querySelector(".date").innerText = day;
-
+let weekly = { weekday: "long" };
+let day = dates.toLocaleDateString("en-Ng", weekly);
+let date = (document.querySelector(".date").innerText = day);
 
 /*
 function updateTime() {
@@ -16,21 +15,21 @@ setInterval(updateTime);
 
 
 */
-        function updateUTCTime() {
-            let currentTime = new Date();
-            let hours = currentTime.getUTCHours();
-            let minutes = currentTime.getUTCMinutes();
-            let seconds = currentTime.getUTCSeconds();
+function updateUTCTime() {
+  let currentTime = new Date();
+  let hours = currentTime.getUTCHours();
+  let minutes = currentTime.getUTCMinutes();
+  let seconds = currentTime.getUTCSeconds();
 
-            // Add leading zeros if necessary
-            hours = hours < 10 ? '0' + hours : hours;
-            minutes = minutes < 10 ? '0' + minutes : minutes;
-            seconds = seconds < 10 ? '0' + seconds : seconds;
+  // Add leading zeros if necessary
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            let timeString = hours + ':' + minutes + ':' + seconds;
-            document.getElementById('time').innerText = timeString;
-        }
-        // Update time immediately
-        updateUTCTime();
-        // Update time every second
-        setInterval(updateUTCTime, 1000);
+  let timeString = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("time").innerText = timeString;
+}
+// Update time immediately
+updateUTCTime();
+// Update time every second
+setInterval(updateUTCTime, 1000);
